64c1ba848ac1b683cabd5e5a4c10eddd405c11c6
saiteja.g <saiteja.g@tallysolutions.com>
2025-01-29 06:58:30 +0000

    Redirect output of backend and frontend server startup to user-data.log for better logging

--- user-data.sh
+++ user-data.sh
@@ -2,13 +2,13 @@
 # Update the package index
 
 # Change directory to the backend path
-cd /home/ec2-user/taggo/backend/
+cd /home/ec2-user/taggo/backend/ >> /home/ec2-user/user-data.log 2>&1
 
 # Run the Django development server
-python manage.py runserver 0.0.0.0:8000 &
+python manage.py runserver 0.0.0.0:8000 >> /home/ec2-user/user-data.log 2>&1 &
 
 # Change directory to the frontend path
-cd /home/ec2-user/taggo/frontend/
+cd /home/ec2-user/taggo/frontend/ >> /home/ec2-user/user-data.log 2>&1
 
 # Run the npm development server
-npm run dev -- -H 0.0.0.0 &
+npm run dev -- -H 0.0.0.0 >> /home/ec2-user/user-data.log 2>&1 &
