70dd9e264f50e6789712c576232ada1caef38f41
saiteja-tally <148423286+saiteja-tally@users.noreply.github.com>
2025-02-18 16:26:14 +0530

    [TAI-1197] Implemented User Dashboard (#4) (#5)
    - Developed a dashboard to display the number of files labeled/reviewed to date,
      with a date filter for each user.
    - Superusers can view the dashboards of all users, with the option to select a user
      from a dropdown to view their progress.
    - The dashboard includes a list of IDs for files labeled/reviewed by the user, along
      with information on who reviewed/labeled the respective invoices, the assignee,
      and the status of each file.
    
    Co-authored-by: navya-tally <166476476+navya-tally@users.noreply.github.com>

--- frontend/app/dashboard/components/SelectUser.tsx
+++ frontend/app/dashboard/components/SelectUser.tsx
@@ -9,7 +9,7 @@ interface SelectUserProps {
 
 const SelectUser: React.FC<SelectUserProps> = ({ selectedUsername, handleUserChange }) => {
     const [groupsWithUsers, setGroupsWithUsers] = useState<{ [key: string]: string[] }>({});
-    
+
 
     useEffect(() => {
         const fetchGroupsWithUsers = async () => {
@@ -19,27 +19,26 @@ const SelectUser: React.FC<SelectUserProps> = ({ selectedUsername, handleUserCha
         fetchGroupsWithUsers();
     }, []
     )
-    
-    
+
     return (
-        <div className="select-user-container">
-            <label htmlFor="user-select" className="select-user-label">Select a user:</label>
-            <select 
-            id="user-select" 
-            className="select-user-dropdown" 
-            onChange={(e) => handleUserChange(e.target.value)}
-            value={selectedUsername ?? ''}
+        <div className="select-user-container flex justify-center p-4 ">
+            <label htmlFor="user-select" className="select-user-label p-2 border border-gray-300 rounded block">Select a user:</label>
+            <select
+                id="user-select"
+                className="select-user-dropdown"
+                onChange={(e) => handleUserChange(e.target.value)}
+                value={selectedUsername ?? ''}
             >
-            <option value="" disabled>--Please choose an option--</option>
-            {Object.entries(groupsWithUsers).map(([group, users]) => (
-                <optgroup key={group} label={group}>
-                {users.map((user: string) => (
-                    <option key={user} value={user}>
-                    {user}
-                    </option>
+                <option value="" disabled>--Please choose an option--</option>
+                {Object.entries(groupsWithUsers).map(([group, users]) => (
+                    <optgroup key={group} label={group}>
+                        {users.map((user: string) => (
+                            <option key={user} value={user}>
+                                {user}
+                            </option>
+                        ))}
+                    </optgroup>
                 ))}
-                </optgroup>
-            ))}
             </select>
         </div>
     );
